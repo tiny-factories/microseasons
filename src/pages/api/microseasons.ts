e50f9d8764114903903a3a97a4c6479b0b81bb1b
William Felker <gndclouds@icloud.com>
2024-03-29 20:28:18 -0700

    cors

--- src/pages/api/microseasons.ts
+++ src/pages/api/microseasons.ts
@@ -2,5 +2,16 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import microseasons from "../../../public/microseasons.json";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Access-Control-Allow-Credentials', false);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
   res.status(200).json(microseasons);
 }
