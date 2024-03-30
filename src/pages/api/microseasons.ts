bcc91fedc991b37ffe1a57fe6408594b0d8b96a3
William Felker <gndclouds@icloud.com>
2024-03-29 20:29:41 -0700

    🐞 🧹 Linting Fix

--- src/pages/api/microseasons.ts
+++ src/pages/api/microseasons.ts
@@ -1,12 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import microseasons from "../../../public/microseasons.json";
-
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.setHeader('Access-Control-Allow-Credentials', false);
+  res.setHeader('Access-Control-Allow-Credentials', 'false');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
+  res.setHeader('Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   );
   if (req.method === 'OPTIONS') {
