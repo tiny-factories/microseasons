a0cbce023b9919085a2e560ed223810f2ead2fe8
William Felker <gndclouds@icloud.com>
2024-04-20 07:55:00 -0700

    changed name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "#Microseasons API",
+  title: "Tiny Seasons",
   description: "Japan Microseasons API  ",
 };
 
